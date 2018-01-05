73b0eb607bfa601a24e47882124d7dcee6a7cfbf
Max Shavrick <maxshavrick@gmail.com>
2018-01-05 09:51:23 +0000

    Now tracking for GEPs. May have to look for other instructions later.

--- mcsema/BC/DeadStoreEliminator.cpp
+++ mcsema/BC/DeadStoreEliminator.cpp
@@ -15,6 +15,10 @@ namespace mcsema {
 
 char mcsema::DeadStoreEliminationPass::ID = 0;
 
+DeadStoreEliminationPass::~DeadStoreEliminationPass() {
+	delete rMap;
+}
+
 bool DeadStoreEliminationPass::runOnBasicBlock(BasicBlock &bb) {
 	errs() << "[Begin Dead Store Elim on BB]\n";
 
@@ -24,13 +28,24 @@ bool DeadStoreEliminationPass::runOnBasicBlock(BasicBlock &bb) {
 		// get all operations that are pointers
 		// if they reference state structure
 		// let's determine the register
-		for (unsigned int i = 0; i < inst->getNumOperands(); i++) {
-			Value *vp = inst->getOperand(i);
-			if (vp->getType()->isPointerTy()) {
-				errs() << *inst << "\n";
-				break;
+		if (GetElementPtrInst *gep = dyn_cast<GetElementPtrInst>(inst)) {
+			// should maybe also check regular instructions
+			// for ops that may reference this?
+			for (unsigned int i = 0; i < gep->getNumOperands(); i++) {
+				errs() << "Operand " << i << ": " << gep->getOperand(i)->getValueName() << "\n";
 			}
+			errs() << *inst << "\n";
 		}
+
+		//for (unsigned int i = 0; i < inst->getNumOperands(); i++) {
+		//	Value *vp = inst->getOperand(i);
+		//	if (vp->getType()->isPointerTy()) {
+		//		// check if references state structure
+		//		// if so, let's move forward 
+		//		//errs() << "[" << vp->getName() << "]\n";
+		//		break;
+		//	}
+		//}
 	}
 
 	errs() << "[End Dead Store Elim on BB]\n";
