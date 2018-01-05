5bb00d97209d5d1f714c27378eea59ff75128b1e
Max Shavrick <maxshavrick@gmail.com>
2018-01-05 10:20:02 +0000

    Add support functions to handle walking def-use chain.
    Also the great re-indendation. Reindented everything to follow the
    2-space UK style.

--- mcsema/BC/DeadStoreEliminator.cpp
+++ mcsema/BC/DeadStoreEliminator.cpp
@@ -16,41 +16,53 @@ namespace mcsema {
 char mcsema::DeadStoreEliminationPass::ID = 0;
 
 DeadStoreEliminationPass::~DeadStoreEliminationPass() {
-	delete rMap;
+  delete rMap;
 }
 
 bool DeadStoreEliminationPass::runOnBasicBlock(BasicBlock &bb) {
-	errs() << "[Begin Dead Store Elim on BB]\n";
+  errs() << "[Begin Dead Store Elim on BB]\n";
 
-	for (auto iter = bb.begin(); iter != bb.end(); iter++) {
-		Instruction *inst = &*iter;
-		
-		// get all operations that are pointers
-		// if they reference state structure
-		// let's determine the register
-		if (GetElementPtrInst *gep = dyn_cast<GetElementPtrInst>(inst)) {
-			// should maybe also check regular instructions
-			// for ops that may reference this?
-			for (unsigned int i = 0; i < gep->getNumOperands(); i++) {
-				errs() << "Operand " << i << ": " << gep->getOperand(i)->getValueName() << "\n";
-			}
-			errs() << *inst << "\n";
-		}
+  for (auto iter = bb.begin(); iter != bb.end(); iter++) {
+    Instruction *inst = &*iter;
+    
+    // get all operations that are pointers
+    // if they reference state structure
+    // let's determine the register
+    if (GetElementPtrInst *gep = dyn_cast<GetElementPtrInst>(inst)) {
+      // should maybe also check regular instructions
+      // for ops that may reference this?
+      Value *vb = gep->getPointerOperand();
+      if (Argument *glob = dyn_cast<Argument>(vb)) {
+        errs() << "FOUND Arg: " << *glob << "\n";
+        // ensure here that this referes to the State structure
+        _attemptDeadLoadRemoval(gep);
+        _attemptDeadStoreRemoval(gep);
+      }
+      errs() << *inst << "\n";
+    }
 
-		//for (unsigned int i = 0; i < inst->getNumOperands(); i++) {
-		//	Value *vp = inst->getOperand(i);
-		//	if (vp->getType()->isPointerTy()) {
-		//		// check if references state structure
-		//		// if so, let's move forward 
-		//		//errs() << "[" << vp->getName() << "]\n";
-		//		break;
-		//	}
-		//}
-	}
+    //for (unsigned int i = 0; i < inst->getNumOperands(); i++) {
+    //  Value *vp = inst->getOperand(i);
+    //  if (vp->getType()->isPointerTy()) {
+    //    // check if references state structure
+    //    // if so, let's move forward 
+    //    //errs() << "[" << vp->getName() << "]\n";
+    //    break;
+    //  }
+    //}
+  }
 
-	errs() << "[End Dead Store Elim on BB]\n";
+  errs() << "[End Dead Store Elim on BB]\n";
 
-	return false;
+  return false;
+}
+
+void DeadStoreEliminationPass::_attemptDeadLoadRemoval(GetElementPtrInst *gep) {
+
+}
+
+void DeadStoreEliminationPass::_attemptDeadStoreRemoval(GetElementPtrInst *gep) {
+
 }
 
 }
--- mcsema/BC/RegisterMap.h
+++ mcsema/BC/RegisterMap.h
@@ -1,66 +1,67 @@
 
 namespace mcsema {
-	
+  
 //
 // TOOD: Move into own file
 class RegisterMap {
-	struct Register {
-		std::string name;
-		size_t size;
-		Register(std::string name_, size_t size_) : name(name_), size(size_) { }
-	};
+  // this class almost definitely needs to be made public
+  struct Register {
+    std::string name;
+    size_t size;
+    Register(std::string name_, size_t size_) : name(name_), size(size_) { }
+  };
 
-	struct RegisterGroup {
-		std::string name;
-		std::vector<Register *> members;
-		RegisterGroup(std::string name_) : name(name_) {}
-		void addRegister(Register *r) {
-			members.push_back(r);
-		}
-	};
+  struct RegisterGroup {
+    std::string name;
+    std::vector<Register *> members;
+    RegisterGroup(std::string name_) : name(name_) {}
+    void addRegister(Register *r) {
+      members.push_back(r);
+    }
+  };
 
-	void addGroup(RegisterGroup *g) {
-		// makes sense to flatten the group here
-		// also makes sense to make this one large template
+  void addGroup(RegisterGroup *g) {
+    // makes sense to flatten the group here
+    // also makes sense to make this one large template
 
-	}
+  }
 
-	Register *registerAtOffset(off_t offset) {
-		// depending on how efficient we want this to be
-		// we'll have to make some changes
-		return NULL;
-	}
+  Register *registerAtOffset(off_t offset) {
+    // depending on how efficient we want this to be
+    // we'll have to make some changes
+    return NULL;
+  }
 
-	public:
-	static RegisterMap *registerMapForTarget(std::string target) {
-		// perhaps use polymorphism here, and return x86RegisterMap
-		// only issue is, for example, some x86 processors have different caps
-		// so I suppose x86RegisterMap can have a cap section.. :(
-		
-		RegisterMap *map = new RegisterMap();
-		
-		if (target == "x86") {
+  public:
+  static RegisterMap *registerMapForTarget(std::string target) {
+    // perhaps use polymorphism here, and return x86RegisterMap
+    // only issue is, for example, some x86 processors have different caps
+    // so I suppose x86RegisterMap can have a cap section.. :(
+    
+    RegisterMap *map = new RegisterMap();
+    
+    if (target == "x86") {
 
-			RegisterGroup *gpr = new RegisterGroup("GPR");
-			
-			gpr->addRegister(new Register("eax", 4));
-			gpr->addRegister(new Register("ebx", 4));
-			gpr->addRegister(new Register("ecx", 4));
-			gpr->addRegister(new Register("edx", 4));
-			gpr->addRegister(new Register("esi", 4));
-			gpr->addRegister(new Register("edi", 4));
-			gpr->addRegister(new Register("esp", 4));
-			gpr->addRegister(new Register("ebp", 4));
-			gpr->addRegister(new Register("eip", 4));
+      RegisterGroup *gpr = new RegisterGroup("GPR");
+      
+      gpr->addRegister(new Register("eax", 4));
+      gpr->addRegister(new Register("ebx", 4));
+      gpr->addRegister(new Register("ecx", 4));
+      gpr->addRegister(new Register("edx", 4));
+      gpr->addRegister(new Register("esi", 4));
+      gpr->addRegister(new Register("edi", 4));
+      gpr->addRegister(new Register("esp", 4));
+      gpr->addRegister(new Register("ebp", 4));
+      gpr->addRegister(new Register("eip", 4));
 
-			map->addGroup(gpr);
-		}
-		else if (target == "x86_64") {
+      map->addGroup(gpr);
+    }
+    else if (target == "x86_64") {
 
-		}
-		// this should be switch/case style
-		return NULL;
-	}
+    }
+    // this should be switch/case style
+    return NULL;
+  }
 };
 
 }
